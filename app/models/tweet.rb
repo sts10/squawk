321b770839f482796599ffaf222ad9c7057d4ffc
sts10 <sschlinkert@gmail.com>
2014-04-14 13:13:43 -0400

    added params and use home_timeline, thanks to Corbin

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
-  def self.get_tweets(this_user)
-    @timeline = MY_TWITTER_CLIENT.user_timeline(this_user)
+  def self.get_tweets
+
+    @timeline = MY_TWITTER_CLIENT.home_timeline(:count => 200)
+
     text_array = []
     @timeline.each do |tweet_obj|
       text_array << tweet_obj.text
