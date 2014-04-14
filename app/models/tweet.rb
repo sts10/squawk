671c036459536c43ea3482181d8c03a9e103c2fa
sts10 <sschlinkert@gmail.com>
2014-04-14 10:13:19 -0400

    trying to fix

---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ActiveRecord::Base
+  def get_tweets(this_user)
+    @timeline = MY_TWITTER_CLIENT.user_timeline(this_user)
+    text_array = []
+    @timeline.each do |tweet_obj|
+      text_array << tweet_obj.text
+    end 
+
+    return text_array
+  end 
+end 
