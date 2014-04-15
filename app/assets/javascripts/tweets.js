eb3ca37d097b0895c4bb23b95fb389c3c09a1689
sts10 <sschlinkert@gmail.com>
2014-04-15 18:27:12 -0400

    jquery to show and hide tweets

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
   $("div#tweets").hide();
+  $("a#hide_tweets").hide();
 
   $("a#show_tweets").on("click", function(e){
-    $("div#tweets").show();
-  })
+    $(this).siblings("div#tweets").slideDown();
+    $(this).hide();
+    $(this).siblings("a#hide_tweets").show();
+
+  });
+
+  $("a#hide_tweets").on("click", function(e){
+    $(this).siblings("div#tweets").slideUp();
+    $(this).siblings("a#show_tweets").show();
+    $(this).hide();
+  });
 });
