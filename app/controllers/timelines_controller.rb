8ebe94977218e0b63a525955bb30d18f026d7d45
sts10 <sschlinkert@gmail.com>
2014-04-16 17:26:13 -0400

    created timeline model, moved tweet class methods to timeline instance methods

---
+++ app/controllers/timelines_controller.rb
@@ -0,0 +1,16 @@
+class TimelinesController < ApplicationController
+  def show
+    
+    @twitter_username = request.env["omniauth.auth"]["info"]["nickname"] # params[:twitter_username]
+    @user_name = request.env["omniauth.auth"]["info"]["name"]
+    @twitter_avatar_url = request.env["omniauth.auth"]["info"]["image"]
+    
+    @timeline = Timeline.new
+    
+    @oauth_token = request.env["omniauth.auth"]["extra"]["access_token"].params[:oauth_token]
+    @oauth_token_secret = request.env["omniauth.auth"]["extra"]["access_token"].params[:oauth_token_secret]
+
+    
+    @url_obj_array = @timeline.make_url_objs(@timeline.make_twitter_client(@oauth_token, @oauth_token_secret))
+  end 
+end
