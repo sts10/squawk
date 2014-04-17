15354010cef0d76c110bc6ff04baf4be2feb3b5b
brian1987 <pisano.b@gmail.com>
2014-04-17 14:48:25 -0400

    expanding stuff

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,17 +1,34 @@
 $(document).ready(function(){
-  $("div#tweets").hide();
-  $("a#hide_tweets").hide();
+  // $("div.tweets").hide();
+  // $("a#hide_tweets").hide();
 
-  $("a#show_tweets").on("click", function(e){
-    $(this).siblings("div#tweets").slideDown();
-    $(this).hide();
-    $(this).siblings("a#hide_tweets").show();
+  // $("a#show_tweets").on("click", function(e){
+  //   $(this).siblings("div.tweets").slideDown();
+  //   $(this).hide();
+  //   $(this).siblings("a#hide_tweets").show();
 
-  });
+  // });
 
-  $("a#hide_tweets").on("click", function(e){
-    $(this).siblings("div#tweets").slideUp();
-    $(this).siblings("a#show_tweets").show();
-    $(this).hide();
-  });
+  // $("a#hide_tweets").on("click", function(e){
+  //   $(this).siblings("div.tweets").slideUp();
+  //   $(this).siblings("a#show_tweets").show();
+  //   $(this).hide();
+  // });
+
+  // alert("this works");
+
+  
+  $("div.individual_tweet").hide();
+
+
+  $("div#tweet_num_0").show();
+
+  $("span.expander").on("click", function(){
+    $(this).siblings("div.individual_tweet").show();
+    $(this).removeClass("glyphicon-chevron-right").hide();
+  })
+
+
+
+
 });
