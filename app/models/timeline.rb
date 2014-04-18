6a7c4765cc7cc58da19bace8844b1f3aae42ad65
sts10 <sschlinkert@gmail.com>
2014-04-18 11:48:45 -0400

    adds 'no squawks found' message

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -29,7 +29,7 @@ class Timeline # < ActiveRecord::Base
     timeline = twitter_client.home_timeline(:count => 199)
     last_id = timeline.last.id - 1
 
-    2.times do 
+    4.times do 
         timeline = timeline + twitter_client.home_timeline(:count => 199, :max_id => last_id)
         i = i + 1
         last_id = timeline.last.id - 1
