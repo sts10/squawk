1c637dc831c97dc773fd1f19a15c3b54bc3c078e
sts10 <sschlinkert@gmail.com>
2014-04-22 10:45:30 -0400

    fixed table width problem

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,36 +1,13 @@
-$(document).ready(function(){
-  // $("div.tweets").hide();
-  // $("a#hide_tweets").hide();
-
-  // $("a#show_tweets").on("click", function(e){
-  //   $(this).siblings("div.tweets").slideDown();
-  //   $(this).hide();
-  //   $(this).siblings("a#hide_tweets").show();
-
-  // });
-
-  // $("a#hide_tweets").on("click", function(e){
-  //   $(this).siblings("div.tweets").slideUp();
-  //   $(this).siblings("a#show_tweets").show();
-  //   $(this).hide();
-  // });
-
-  // alert("this works");
-
-  
+$(document).ready(function(){ 
   $(".individual_tweet").hide();
 
-  // $(".number_of_times").show();
+  $(".number_of_times").show();
   $(".tweet_num_0").show();
-  // $(".arrow").show();
 
   $("td.expander").on("click", function(){
     $(this).siblings(".individual_tweet").show();
     $(this).removeClass("glyphicon-chevron-right").hide();
   });
-
-  // $('div#carousel-example-generic').carousel()
-
 
 
 });
